b63c32e409c68b7bd2c1f2a8782627785f637ac0
Mei08 <125234mary@gmail.com>
2016-02-15 23:33:50 -0500

    header

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mei He' });
 });
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'contact' });
+});
+
 
 module.exports = router;
